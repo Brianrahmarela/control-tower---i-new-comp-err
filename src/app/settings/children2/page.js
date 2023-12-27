4ff3260e03efcf95827ada46637b70677ac3c37e
Brian <brian.rahmarela@gmail.com>
2023-12-27 18:45:52 +0700

    first init

---
+++ src/app/settings/children2/page.js
@@ -0,0 +1,13 @@
+import { Separator } from "@/components/ui/separator";
+
+const Children2 = () => {
+  return (
+    <div className="flex h-full flex-col space-y-4 p-4 pt-6 mdp-8">
+      Children 2 settings
+      <Separator />
+      <div className="flex-1 rounded"></div>
+    </div>
+  );
+};
+
+export default Children2;
---
+++ src/app/users/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Users = () => {
+    return (
+        <div className='flex h-full flex-col space-y-4 p-4 pt-6 mdp-8'>
+            Page Users
+        </div>
+    )
+}
+
+export default Users
